3a9e35dc6dd6bac29bda3adeca5fdaa33f682630
sourcebrowse <sourcebrowse@gmail.com>
2015-01-16 14:28:31 +0100

    changed file endings in obt

--- obt.py
+++ obt.py
@@ -10,7 +10,7 @@ def tag(file_path):
 proc_list = []
 for root, dirs, files in os.walk(sys.argv[1]):
     for f in files:
-        if f.endswith('.txt') and not f.endswith('_metadata.txt') and not f.endswith('pdf') and not f.endswith('obt'):
+        if f.endswith('.txt') and not f.endswith('_metadata.txt') and not f.endswith('.pdf') and not f.endswith('.obt'):
             p = os.path.join(root, f)
             print p
             process = mp.Process(target=tag, args=(p, ))
